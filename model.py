a045c2d3658a30c98ac3c19d763b3c74f2744dcc
azonecloud <100863730+azonecloud@users.noreply.github.com>
2025-04-02 05:51:12 +0530

    Latest Commits

--- model.py
+++ model.py
@@ -5,7 +5,6 @@ from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
 import pickle
 
-
 # Preprocess the data
 # Load the dataset
 df = pd.read_csv('data/rental_1000.csv')
@@ -22,8 +21,8 @@ lr = LinearRegression()
 model = lr.fit(X_train, y_train)
 
 # Evaluate the model
-predictions = model.predict(X_test)
-print("Actual Prices Vs Predicted Prices ", y_test[:5], predictions[:5])
+#predictions = model.predict(X_test)
+#print("Actual Prices Vs Predicted Prices ", y_test[:5], predictions[:5])
 
 score = model.score(X_test, y_test)
 rmse = np.sqrt(np.mean((model.predict(X_test) - y_test) ** 2))
@@ -34,14 +33,4 @@ print(f"RMSE: {rmse}")
 with open('model/model.pkl', 'wb') as file:
     pickle.dump(model, file)
 
-# Predict using the model
-def predict_price(rooms, area):
-    # Load the model
-    with open('model/model.pkl', 'rb') as file:
-        model = pickle.load(file)
-
-    # Make a prediction
-    prediction = model.predict([[rooms, area]])
-    return prediction[0]
-
-print(predict_price(3, 120))  # Example prediction for 3 rooms and 120 area
+print("Model Trained and Saved successfully!")
